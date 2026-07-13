6c3b0070a1694ffb0d80ab7cc0591e694dc93a02
bestrever <dasaoo@proton.me>
2026-07-13 08:31:22 +0800

    Update feed sources in diy-part1.sh
    Added new feed sources for iStore and updated Passwall repository links.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 添加 iStore 源
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 替换 Passwall 仓库为 Openwrt-Passwall (包含核心与依赖)
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
+
+# 注：Mihomo, MosDNS, SSR-Plus, HomeProxy 和 CloudflareSpeedtest 
+# 通常已包含在 ImmortalWrt 24.10 的自带源中，无需额外引入。
